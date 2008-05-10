882bbff6909e74d2d27392b9856a48e9c8b40b27
shinysky1986 <shinysky1986@20dc0312-9647-0410-8b6d-03bcedc6537e>
2008-05-10 05:41:04 +0000

    2008.05.10 Begin PeerGroup Programming

---
+++  projectnox/src/noxUI/AuthenticationPanel.java
@@ -0,0 +1,44 @@
+package noxUI;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import net.jxta.exception.PeerGroupException;
+import net.jxta.peergroup.PeerGroup;
+
+/**
+ * 用于加入组时输入密码?
+ * @author shinysky
+ *
+ */
+@SuppressWarnings("serial")
+public class AuthenticationPanel extends JPanel {
+
+	public AuthenticationPanel(JFrame parent, PeerGroup pg) {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isCanceled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isReadyForJoin() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void join() throws PeerGroupException{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
